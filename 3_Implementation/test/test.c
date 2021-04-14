38e70d50a999f1342f2d7cecefff0cc139f274b6
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-14 21:00:35 +0530

    Add files via upload

---
+++ 3_Implementation/test/test.c
@@ -0,0 +1,28 @@
+#include"../unity/unity.h"
+#include"../unity/unity_internals.h"
+#include"../inc/worldtime.h"
+#include"../inc/currency.h"
+void setUp(void)
+{
+}
+void tearDown(void)
+{
+}
+void test_should_convert(void){
+    float rupee=204.2;
+    TEST_ASSERT_EQUAL_FLOAT(10,currency(2,&rupee));
+    float rupee1=225;
+    TEST_ASSERT_EQUAL_FLOAT(3,currency(6,&rupee1));
+    float rupee2=69;
+    TEST_ASSERT_EQUAL_FLOAT(100,currency(8,&rupee2));
+    float rupee3=20.60;
+    TEST_ASSERT_EQUAL_FLOAT(4,currency(11,&rupee3));
+    float rupee4=67;
+    TEST_ASSERT_EQUAL_FLOAT(1000,currency(19,&rupee4));
+}
+int main(void)
+{
+UNITY_BEGIN();
+RUN_TEST(test_should_convert);
+return UNITY_END();
+}
