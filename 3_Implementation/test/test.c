bc072a3f1ab17bf6297e33451bd3963101be3b02
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-15 13:11:01 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -11,15 +11,15 @@ void tearDown(void)
 }
 void test_should_convert(void){
     float rupee=204.2;
-    TEST_ASSERT_EQUAL(10,currency(2,&rupee));
+    TEST_ASSERT_EQUAL_FLOAT(10,currency(2,&rupee));
     float rupee1=225;
-    TEST_ASSERT_EQUAL(3,currency(6,&rupee1));
+    TEST_ASSERT_EQUAL_FLOAT(3,currency(6,&rupee1));
     float rupee2=69;
-    TEST_ASSERT_EQUAL(100,currency(8,&rupee2));
+    TEST_ASSERT_EQUAL_FLOAT(100,currency(8,&rupee2));
     float rupee3=20.60;
-    TEST_ASSERT_EQUAL(4,currency(11,&rupee3));
+    TEST_ASSERT_EQUAL_FLOAT(4,currency(11,&rupee3));
     float rupee4=67;
-    TEST_ASSERT_EQUAL(1000,currency(19,&rupee4));
+    TEST_ASSERT_EQUAL_FLOAT(1000,currency(19,&rupee4));
 }
 int main(void)
 {
